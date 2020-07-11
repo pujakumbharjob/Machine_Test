a4ee5eb9c9b2e20a4c12e6d72b1215bf107b5f80
Abhishek Dixit <abhishekd9519@gmail.com>
2020-07-11 12:09:26 +0530

    ProductPersister is added and ProductBusiness List is added

---
+++ BusinessLayer/Machine_Test.Persister/BusinessList/ProductList.cs
@@ -0,0 +1,61 @@
+﻿using Machine_Test.Dal;
+using Machine_Test.IDal;
+using Machine_Test.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Machine_Test.Persister
+{
+    public class ProductList
+    {
+
+        public static List<Product> GetList(Int32 ProductId)
+        {
+            return Fetch(ProductId);
+        }
+
+        private static List<Product> Fetch(Int32 ProductId)
+        {
+            IProductDal IproductDal = new ProductDal();
+            List<Product> productList = new List<Product>();
+            foreach (var item in IproductDal.GetList(ProductId))
+            {
+                Product productDto = new Product
+                {
+                    Id = item.Id,
+                    ProductId = item.ProductId,
+                    ProductAttribute=item.ProductAttribute,
+                    ProductDetails=item.ProductDetails
+                };
+                productList.Add(productDto);
+            }
+            return productList;
+        }
+
+        public static List<Product> GetPIVOTList()
+        {
+            return fetPIVOTList();
+        }
+
+        private static List<Product> fetPIVOTList()
+        {
+            IProductDal IproductDal = new ProductDal();
+            List<Product> productList = new List<Product>();
+            foreach (var item in IproductDal.GetPIVOTList())
+            {
+                Product productDto = new Product
+                {
+                  
+                    ProductId = item.ProductId,
+                    Name = item.Name,
+                    Price = long.Parse(item.Price.ToString())
+                };
+                productList.Add(productDto);
+            }
+            return productList;
+        }
+    }
+}
---
+++ BusinessLayer/Machine_Test.Persister/Persister/ProductPersister.cs
@@ -0,0 +1,106 @@
+﻿using Machine_Test.Dal;
+using Machine_Test.IDal;
+using Machine_Test.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Machine_Test.Persister
+{
+    public class ProductPersister
+    {
+        public Product Insert(Product product, SqlConnection con = null, SqlTransaction trans = null)
+        {
+            IProductDal IproductDal = new ProductDal();
+            ProductDto productNameDto = new ProductDto
+            {
+                ProductAttribute ="Name",            
+                ProductDetails = product.Name,
+                ProductId= IproductDal.GetHighestProductId()+1,
+                IsActive =true
+            };
+            IproductDal.Insert(productNameDto);
+            ProductDto productPriceDto = new ProductDto
+            {
+                ProductAttribute ="Price",
+                ProductDetails = product.Price.ToString(),
+                ProductId = productNameDto.ProductId,
+                IsActive = true
+            };
+            IproductDal.Insert(productPriceDto);
+
+            return product;
+        }
+
+        public Product Update(Product product, SqlConnection con = null, SqlTransaction trans = null)
+        {
+            IProductDal IproductDal = new ProductDal();
+            ProductDto productNameDto = new ProductDto
+            {
+               
+                ProductAttribute = "Name",
+                ProductDetails = product.Name,
+                ProductId = product.ProductId,
+                IsActive = true
+            };
+            IproductDal.UpdateName(productNameDto);
+            ProductDto productPriceDto = new ProductDto
+            {
+                
+                ProductAttribute = "Price",
+                ProductDetails = product.Price.ToString(),
+                ProductId = product.ProductId,
+                IsActive = true
+            };
+            IproductDal.UpdatePrice(productPriceDto);
+
+            return product;
+        }
+
+
+        public Product Delete(Product product)
+        {
+            IProductDal IproductDal = new ProductDal();
+            ProductDto productDto = new ProductDto
+            {
+                Id = product.Id,
+                ProductId=product.ProductId,
+                IsActive = false
+            };
+            IproductDal.Delete(productDto);
+            
+            return product;
+        }
+        public static Product GetProduct(Int32 ProductId)
+        {
+            IProductDal IproductDal = new ProductDal();
+            List<Product> productDtoList = ProductList.GetList(ProductId);
+            Product product = new Product();
+            product.ProductId = ProductId;
+            product.Name = productDtoList.Where(a => a.ProductAttribute == "Name").FirstOrDefault().ProductDetails;
+            product.Price = long.Parse(productDtoList.Where(a => a.ProductAttribute == "Price").FirstOrDefault().ProductDetails);
+
+            return product;
+        }
+
+
+
+        public static Product Get()
+        {
+            Product product = new Product();
+            return product;
+        }
+
+        public static ProductPersister GetPersister()
+        {
+            ProductPersister productPersister = new ProductPersister();
+            return productPersister;
+        }
+
+
+    }
+}
