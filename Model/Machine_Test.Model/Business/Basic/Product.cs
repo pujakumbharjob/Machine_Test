8a82a13dcb59e14af4a787043d7dcc097b6d9c64
Abhishek Dixit <abhishekd9519@gmail.com>
2020-07-11 11:59:50 +0530

    Model is Created and Product Class is added.

---
+++ Model/Machine_Test.Model/Business/Basic/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Machine_Test.Model
+{
+    public class Product
+    {
+        public Int32 Id { get; set; }
+
+        public Int32 ProductId { get; set; }
+
+        public String Name { get; set; }
+
+        public long Price { get; set; }
+
+        public Boolean IsActive { get; set; }
+
+        public String ProductAttribute { get; set; }
+
+        public String ProductDetails { get; set; }
+
+    }
+}
