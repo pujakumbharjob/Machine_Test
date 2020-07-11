8dd30a1c5e4c674f0387003396299b0a7dde1490
Abhishek Dixit <abhishekd9519@gmail.com>
2020-07-11 12:02:08 +0530

    Product DTO Class is added and Product Interface is added.

---
+++ DataLayer/Machine_Test.IDal/DTO/ProductDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Machine_Test.IDal
+{
+   public class ProductDto
+    {
+        public Int32 Id { get; set; }
+
+        public Int32 ProductId { get; set; }
+
+        public String ProductAttribute { get; set; }
+
+        public String ProductDetails { get; set; }
+
+        public Boolean IsActive { get; set; }
+
+    }
+}
---
+++ DataLayer/Machine_Test.IDal/IDAL/IProductDal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Machine_Test.IDal
+{
+    public interface IProductDal
+    {
+        List<ProductDto> GetList(Int32 ProductId);
+
+        ProductDto GetProduct(Int32 Id);
+
+        Int32  GetHighestProductId();
+
+        ProductDto Insert(ProductDto productDto);
+
+        ProductDto UpdateName(ProductDto productDto);
+
+        ProductDto UpdatePrice(ProductDto productDto);
+
+        List<ProductUpdateDto> GetPIVOTList();
+
+       ProductDto Delete(ProductDto productDto);
+    }
+}
